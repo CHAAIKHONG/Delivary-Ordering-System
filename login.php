b5678c9de141e22a5a4103f4555ff3a07629ab87
TANG YUT HEE <1221206903@soffice.mmu.edu.my>
2024-07-05 16:53:21 +0800

    hello

--- login.php
+++ login.php
@@ -4,11 +4,6 @@
     // 连接到数据库
     $connect = mysqli_connect("localhost", "root", "", "moonbeedb");
 
-    // 检查连接
-    if (!$connect) {
-        die("Database connection failed: " . mysqli_connect_error());
-    }
-
     // 处理登录表单提交
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $email = mysqli_real_escape_string($connect, $_POST['username']);
@@ -21,11 +16,10 @@
 
         if (mysqli_num_rows($result) > 0) {
             $user = mysqli_fetch_assoc($result);
-
-            // 登录成功，设置会话
+            
             $_SESSION['username'] = $email;
             $_SESSION['user_id'] = $user['user_id'];
-            $_SESSION['fullname'] = $user['first_name'] . ' ' . $user['last_name'];
+            $_SESSION['fullname'] = $user['first_name'].''.$user['last_name'];
             $_SESSION['photo'] = $user['user_photo'];
 
             // 如果选择了Remember Me，设置Cookie
@@ -35,16 +29,9 @@
                 setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 30天有效期的Cookie
             }
 
-            // 检查是否为管理员
-            if ($email === 'admin@gmail.com' && $password === 'admin1234') {
-                // 重定向到管理员主页
-                header('Location: mainmenu.html'); // 替换成你的管理员主页地址
-                exit;
-            } else {
-                // 重定向到用户主页
-                header('Location: mainmenu.html'); // 替换成你的主页地址
-                exit;
-            }
+            // 重定向到其他页面，例如主页
+            header('Location: menu.php'); // 替换成你的主页地址
+            exit;
         } else {
             // 登录失败，可以添加错误提示
             echo '<script>alert("Invalid email or password")</script>';
@@ -58,6 +45,7 @@
 
     mysqli_close($connect);
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
